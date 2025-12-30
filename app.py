afb88cbaceef888f674d064555418b86426f9d3e
Kaviya <kaviya290529@gmail.com>
2025-12-30 16:45:13 +0530

    Save local changes before pull

--- app.py
+++ app.py
@@ -269,9 +269,10 @@ def view_paste(paste_id):
     db.session.commit()
     return render_template("view_paste.html", content=paste.content)
 
-# 1️⃣1️⃣ Run the app
+import os
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port=int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
 
